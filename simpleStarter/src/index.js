b973fba221152e071ea6ea58c950079ee9f10be2
Andrew <andrew.j.heinke@gmail.com>
2017-04-06 19:59:16 -0500

    rendering main video area and video list using api

--- simpleStarter/src/index.js
+++ simpleStarter/src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyA_GOBsQy02FlsvC8QGaFV-Vew0qg2WEbI';
 
-// create a new component
-// this component should produce html
-const App = () => {
-  return <div>Andrew is awesome!</div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos: videos});
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 
